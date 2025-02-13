148399b5b40e416747b62982a663807615630639
Jimmy <toluadejimi@gmail.com>
2025-02-13 16:27:29 +0100

    pijl

--- core/public/index.php
+++ core/public/index.php
@@ -33,16 +33,7 @@ if (file_exists($maintenance = __DIR__.'/core/storage/framework/maintenance.php'
 
 require __DIR__.'/core/vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+
 
 $app = require_once __DIR__.'/core/bootstrap/app.php';
 
